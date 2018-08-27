cdd7dc8009abb1134ca32bb054ad60589be7b6bc
Jeovane Santos <juniojsv@gmail.com>
2018-08-27 18:02:01 -0300

    Codigo melhorado, e foram resolvidos alguns erros de ortografia.

--- Codigo_fonte/Codigo_fonte.ino
+++ Codigo_fonte/Codigo_fonte.ino
@@ -1,8 +1,9 @@
 #include <DS1307.h>
 #include <LiquidCrystal.h>
 
-//Autores: Jeovane Santos Vasconcelos e Joel, Colegio Dr. Antonio Ricaldi 2018;
+//Autores: Jeovane Santos e Joel Vasconcelos, Colegio Dr. Antonio Ricaldi 2018;
 
+//INSTANCIAR OBJETOS;
 LiquidCrystal TELA(12, 11, 5, 4, 3, 2);
 DS1307 RELOGIO(A4, A5);
 unsigned long UM_SEGUNDO = 1000L;
@@ -27,15 +28,13 @@ void ligarBomba(bool VARIAVEL) {
     //DELAY DE 1 MINUTO;
     delay(1*UM_MINUTO);
   } else if(!VARIAVEL) {
+    //DESLIGAR BOM D'AQUA;
     TELA.print("Bomba d'aqua desligada.");
   }
 }
 
 void loop() {
-  if(RELOGIO.getTimeStr(FORMAT_LONG) == "17:00:00"){
-    ligarBomba(true);
-    ligarBomba(false);
-  } else if(RELOGIO.getTimeStr(FORMAT_LONG) == "07:00:00") {
+  if(RELOGIO.getTimeStr(FORMAT_LONG) == "07:00:00" || "17:00:00"){
     ligarBomba(true);
     ligarBomba(false);
   }
